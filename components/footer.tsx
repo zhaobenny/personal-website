035893cde7f48953026fc1276086f962fe587f21
Benny Zhao <benny_zhao@sfu.ca>
2021-10-15 15:55:38 -0700

    Fix PDF sizing issue

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer(){
       <footer className="w-full text-grey-500 dark:text-grey-400 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-transparent rounded-full" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-black dark:text-white p-2 bg-transparent rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">Bottom Text</span>
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,18 +1,39 @@
 import Head from 'next/head'
-import React from 'react';
-import { Page, Document, pdfjs } from 'react-pdf'
+import React, { useEffect, useRef, useState} from 'react';
+import { Page, Document, pdfjs} from 'react-pdf'
+
+
+function useWidth() {
+  const [widthSize, setWidthSize] = useState(1000);
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWidthSize(window.innerWidth);
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return widthSize;
+}
 
 const Resume = () => {
   pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
-  return (
+  const documentWrapperRef = useRef<HTMLDivElement>(null);
+  const size = useWidth();
+  return(
     <>
       <Head>
         <title>Benny Zhao | Resume</title>
       </Head>
-      <div className='flex flex-col items-center mt-3'>
+      <div className='flex flex-col items-center mt-2' ref={documentWrapperRef}>
         <a href='./Resume.pdf' download='./Resume.pdf' target='_blank'>
           <Document file="./Resume.pdf" className='border-gray-800 dark:border-0 border-4'>
-            <Page pageNumber={1} />
+            <Page className="w-full" pageNumber={1} width={size/2.5 || 500}/>
           </Document>
           <button className="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none rounded text-lg dark:mt-0 mt-3 hover:opacity-90" >Save as PDF</button>
         </a>
